4b7de3e45466a59a0e4e4bf92aea35fb88354721
yifnzhao <yifan.zhao@mail.mcgill.ca>
2020-05-14 19:26:48 -0400

    Update example.py

--- src/example.py
+++ src/example.py
@@ -1,15 +1,20 @@
-from register import superregister
+from register import register, combine
+import os
 
 # example usage
 
 # declare path
 folder = '../data/multichannel/'
-hr_tiff_path = 'u_germline_hr.tif'
-lr_tiff_path = 'u_germline_lr.tiff'
+hr_tiff = 'germline_hr.tif'
+lr_tiff = 'germline_lr.tiff'
+hr_output = 'r_'+hr_tiff
+lr_output = 'r_'+lr_tiff
+# You can also separate inout and output by giving them full path 
+
 # specify where you put your csv's (can be different from the folder above)
 csv_path = folder
 
-# change working directory
+# change working directory  (if you used relative path rather than absolute path)
 os.chdir(folder)
 
 # specify the number of roi ("cell") you saved
@@ -19,15 +24,11 @@ n_roi = 2
 trans_mat = combine(csv_path, n_csv = n_roi)
 
 # 1) register a low res movie (if you generated the ROI csv using compressed movie)
-metadata = register(tiff_path,trans_mat,out_tiff_path,highres = False,compress = 3, pad = True)
+metadata = register(lr_tiff,trans_mat,lr_output,highres = False,compress = 3, pad = True)
     
-# register a high res movie with the same matrix
-super_register(folder,tiff_path=hr_tiff_path,n_roi=2,high_res=True,compress=3)
+# 2) register a high res movie with the same matrix
+metadata = register(hr_tiff,trans_mat,hr_output,highres = True,compress = 3, pad = True)
 
-# if you used the original movie to generate the ROI csv, set compress=1
-super_register(folder,tiff_path=hr_tiff_path,n_roi=2,high_res=True,compress=1)
+# 3) if you used the original movie to generate the ROI csv, set compress=1
+metadata = register(hr_tiff,trans_mat,hr_output,highres = True,compress = 1, pad = True)
 
-    
-trans_mat = combine(csv_path, n_csv = n_roi)
-    metadata = register(tiff_path,trans_mat,out_tiff_path,highres = False,compress = 3, pad = True)
-    return metadata   
